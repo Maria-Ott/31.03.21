bcbe1506c82049b13249bfc00cae064c6f255780
Maria-Ott <81133160+Maria-Ott@users.noreply.github.com>
2021-05-24 22:10:19 +0700

    Add files via upload

---
+++ 31.03.21.js
@@ -0,0 +1,36 @@
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+movies: {
+    'logan: 87'
+};
+if(numberOfFilms)
+{
+    console.log(numberOfFilms)
+};
+const personalMovieDB = {
+    const: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+if(personalMovieDB)
+{
+    console.log(personalMovieDB);
+};
+const a = prompt("Один из последних просмотренных фильмов?")
+movies: {
+    'logan: Сумерки'
+};
+b = prompt("На сколько оцените его?"), 
+c = prompt("Один из последних просмотренных фильмов?"),
+d = prompt("На сколько оцените его?");
+movies: {
+    'logan: 10 баллов'
+};
+
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
+if(personalMovieDB)
+{
+    console.log(personalMovieDB);
+};
